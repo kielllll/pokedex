36e3be0e2f18e803db02159003145cda6b034556
kielllll <eatarranza@gmail.com>
2024-08-15 18:14:32 +0800

    refactor: make type tag reusable

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -5,11 +5,11 @@ import {
   Flex,
   Heading,
   Image,
-  Tag,
   Text,
 } from '@chakra-ui/react'
 import { useGetPokemon } from '../../queries/pokemons'
 import { PokemonResponse } from '../../app_provider'
+import Tag from '../tag'
 
 interface ICardProps {
   pokemon: PokemonResponse
@@ -20,27 +20,6 @@ const digits4 = new Intl.NumberFormat(undefined, {
   maximumSignificantDigits: 4,
   useGrouping: false,
 })
-
-const COLORS: Record<string, string> = {
-  normal: '#A8A77A',
-  fire: '#EE8130',
-  water: '#6390F0',
-  electric: '#F7D02C',
-  grass: '#7AC74C',
-  ice: '#96D9D6',
-  fighting: '#C22E28',
-  poison: '#A33EA1',
-  ground: '#E2BF65',
-  flying: '#A98FF3',
-  psychic: '#F95587',
-  bug: '#A6B91A',
-  rock: '#B6A136',
-  ghost: '#735797',
-  dragon: '#6F35FC',
-  dark: '#705746',
-  steel: '#B7B7CE',
-  fairy: '#D685AD',
-}
 
 const IMAGE_PLACEHOLDER =
   'https://i.pinimg.com/originals/f5/54/89/f5548916ca86b30f7b8f418e4c5c6794.png'
@@ -70,17 +49,9 @@ export default function Card(props: ICardProps) {
       </CardBody>
       <CardFooter>
         <Flex alignItems="center" gap={4}>
-          {data?.types.map(({ type }: { type: Record<string, string> }) => {
-            const { name } = type
-            const color = COLORS[name]
-            const capitalizedType = name.charAt(0).toUpperCase() + name.slice(1)
-
-            return (
-              <Tag key={name} backgroundColor={color}>
-                {capitalizedType}
-              </Tag>
-            )
-          })}
+          {data?.types.map(({ type }: { type: Record<string, string> }) => (
+            <Tag name={type.name} key={type.name} />
+          ))}
         </Flex>
       </CardFooter>
     </ChakraCard>
