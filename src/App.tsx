b672d56f11b0c4ae671d97d7519fbc76852850aa
kielllll <eatarranza@gmail.com>
2024-08-15 10:28:48 +0800

    chore: setup tanstack react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { RouterProvider } from 'react-router-dom'
-import { routes } from './routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ChakraProvider } from '@chakra-ui/react'
+import { routes } from './routes'
 import { AppProvider } from './app_provider'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <ChakraProvider>
-      <AppProvider>
-        <RouterProvider router={routes} />
-      </AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <RouterProvider router={routes} />
+        </AppProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
