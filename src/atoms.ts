92506eeaef0b14bcff1a33f52bad74a01364ae5e
kielllll <eatarranza@gmail.com>
2024-08-17 13:17:35 +0800

    feat: prepare local storage

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,3 +1,4 @@
 import { atomWithStorage } from 'jotai/utils'
+import { Pokemon } from './queries/pokemons'
 
-export const pokemonsAtom = atomWithStorage('pokemons', [])
+export const pokemonsAtom = atomWithStorage<Pokemon[]>('pokemons', [])
