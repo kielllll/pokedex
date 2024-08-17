eeb3b776150b0d1322f48c46b58549440fab80a4
kielllll <eatarranza@gmail.com>
2024-08-17 13:38:38 +0800

    feat: create reusable select field

---
+++ src/components/fields/Select/index.tsx
@@ -0,0 +1,34 @@
+import { FormControl, FormLabel, Text } from '@chakra-ui/react'
+import { OptionBase, Select } from 'chakra-react-select'
+
+export interface OptionGroup extends OptionBase {
+  label: string
+  value: string
+}
+
+interface ISelectFieldProps {
+  label: string
+  name: string
+  value: OptionGroup[]
+  options: OptionGroup[]
+  errorMessage?: string
+  isMulti?: boolean
+  onChange: (values: OptionGroup[]) => void
+  isLoading?: boolean
+}
+
+export default function SelectField(props: ISelectFieldProps) {
+  return (
+    <FormControl>
+      <FormLabel>{props.label}</FormLabel>
+      <Select
+        options={props.options}
+        isMulti={props.isMulti}
+        onChange={(values) => props.onChange(values as OptionGroup[])}
+        value={props.value}
+        isDisabled={props.isLoading}
+      />
+      {props.errorMessage && <Text color="red.500">{props.errorMessage}</Text>}
+    </FormControl>
+  )
+}
